2075511dd6d242ef2adaed71af5d63420eda9dcf
rhnsharma999 <rhnsharma999@gmail.com>
2017-06-20 19:23:38 +0530

    Initial Commit

---
+++ fantasyFootball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fantasyFootball
+//
+//  Created by Rohan Lokesh Sharma on 20/06/17.
+//  Copyright © 2017 webarch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
